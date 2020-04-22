c65d53428c0f70951cb5b6ee8463fee91b4a1ed6
quaresmateo <teoian.giovane@gmail.com>
2020-04-22 13:09:43 -0300

    Cria componente de busca de produtos

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ProdutosBuscar",
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
+};
+</script>
+
+<style></style>
