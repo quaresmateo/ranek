8bca30192ac8c703acef933a267d6aed34cf8da8
quaresmateo <teoian.giovane@gmail.com>
2020-04-23 08:50:15 -0300

    Estiliza o campo de busca da home

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,6 +1,12 @@
 <template>
   <form>
-    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="text"
+      name="busca"
+      id="busca"
+      v-model="busca"
+      placeholder="Buscar"
+    />
     <input
       type="submit"
       id="lupa"
@@ -26,4 +32,36 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:hover,
+#busca:focus {
+  outline: none;
+  transform: scale(1.1);
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+}
+
+#lupa {
+  background: url("../assets/search.svg") no-repeat center center;
+  border: none;
+  box-shadow: none;
+  cursor: pointer;
+  height: 62px;
+  position: absolute;
+  width: 60px;
+  right: 0px;
+  text-indent: -150px;
+  top: 0px;
+}
+</style>
