2011b18c8859855e45fa01a4b9fb51da795fc438
quaresmateo <teoian.giovane@gmail.com>
2020-05-01 09:45:40 -0300

    Cria plugin para form de usuário

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -2,31 +2,22 @@
   <form>
     <label for="nome">Nome</label>
     <input id="nome" name="nome" type="text" v-model="nome" />
-
     <label for="email">Email</label>
     <input id="email" name="email" type="email" v-model="email" />
-
     <label for="senha">Senha</label>
     <input id="senha" name="senha" type="password" v-model="senha" />
-
     <label for="cep">Cep</label>
     <input id="cep" name="cep" type="text" v-model="cep" />
-
     <label for="rua">Rua</label>
     <input id="rua" name="rua" type="text" v-model="rua" />
-
     <label for="numero">Numero</label>
     <input id="numero" name="numero" type="text" v-model="numero" />
-
     <label for="bairro">Bairro</label>
     <input id="bairro" name="bairro" type="text" v-model="bairro" />
-
     <label for="cidade">Cidade</label>
     <input id="cidade" name="cidade" type="text" v-model="cidade" />
-
     <label for="estado">Estado</label>
     <input id="estado" name="estado" type="text" v-model="estado" />
-
     <div class="button">
       <slot></slot>
     </div>
@@ -34,8 +25,27 @@
 </template>
 
 <script>
+import { mapFields } from "@/helpers.js";
+
 export default {
-  name: "UsuarioForm"
+  name: "UsuarioForm",
+  computed: {
+    ...mapFields({
+      fields: [
+        "nome",
+        "email",
+        "senha",
+        "rua",
+        "cep",
+        "numero",
+        "bairro",
+        "cidade",
+        "estado",
+      ],
+      base: "usuario",
+      mutation: "UPDATE_USUARIO",
+    }),
+  },
 };
 </script>
 
