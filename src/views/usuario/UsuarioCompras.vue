027382387c3f5729c017c213b4ea5882f36cc8db
quaresmateo <teoian.giovane@gmail.com>
2020-05-04 12:13:11 -0300

    Cria rotas de usuário

---
+++ src/views/usuario/UsuarioCompras.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Compras</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/usuario/UsuarioProdutos.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Lista de produtos</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
