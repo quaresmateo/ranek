efd3a9bfad3d0660ff72e61543f257434291cc8c
quaresmateo <teoian.giovane@gmail.com>
2020-05-05 17:28:12 -0300

    Cria página de produtos do usuário

--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -1,9 +1,50 @@
 <template>
-  <p>Lista de produtos</p>
+  <section>
+    <h2>Adicionar Produto</h2>
+    <ProdutoAdicionar />
+    <h2 @click="getUsuarioProdutos">Seus Produtos</h2>
+
+    <transition-group v-if="usuario_produtos" name="list" tag="ul">
+      <li v-for="(produto, index) in usuario_produtos" :key="index">
+        <ProdutoItem :produto="produto">
+          <p>{{ produto.descricao }}</p>
+        </ProdutoItem>
+      </li>
+    </transition-group>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import { mapState, mapActions } from "vuex";
+export default {
+  name: "UsuarioProduto",
+  components: {
+    ProdutoAdicionar,
+    ProdutoItem,
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"]),
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"]),
+  },
+  watch: {
+    login() {
+      this.getUsuarioProdutos();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getUsuarioProdutos();
+    }
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+</style>
