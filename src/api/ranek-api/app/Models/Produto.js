43af8e7c0b99b1ca81d0aeafc94c0dd8c44ce307
quaresmateo <teoian.giovane@gmail.com>
2020-05-26 11:23:18 -0300

    Adiciona trait para slugs de produtos

--- src/api/ranek-api/app/Models/Produto.js
+++ src/api/ranek-api/app/Models/Produto.js
@@ -1,9 +1,19 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class Produto extends Model {
+  static boot() {
+    super.boot();
+
+    this.addTrait("@provider:Lucid/Slugify", {
+      fields: {
+        slug: "nome"
+      },
+      strategy: "dbIncrement"
+    });
+  }
 }
 
-module.exports = Produto
+module.exports = Produto;
