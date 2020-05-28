d945fbd28092465e5578414c3886ec08334b9784
quaresmateo <teoian.giovane@gmail.com>
2020-05-28 11:06:51 -0300

    Cria rota para pegar imagens

--- src/api/ranek-api/start/routes.js
+++ src/api/ranek-api/start/routes.js
@@ -22,4 +22,8 @@ Route.group(() => {
   Route.post("/login", "UserController.login");
   Route.get("/me", "UserController.me").middleware("auth:jwt");
   Route.post("/produtos", "ProdutoController.store").middleware("auth:jwt");
+  Route.post("produtos/:id/images", "ImageController.store").middleware(
+    "auth:jwt"
+  );
+  Route.get("images/:path", "ImageController.show");
 }).prefix("/api/v1");
