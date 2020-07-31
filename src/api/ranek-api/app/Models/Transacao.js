7bd755368779e63fd31e74fbf339f7a40dba2236
quaresmateo <teoian.giovane@gmail.com>
2020-07-31 11:57:02 -0300

    Cria model de transação

---
+++ src/api/ranek-api/app/Models/Transacao.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+
+// eslint-disable-next-line no-undef
+const Model = use("Model");
+
+class Transacao extends Model {}
+
+module.exports = Transacao;
