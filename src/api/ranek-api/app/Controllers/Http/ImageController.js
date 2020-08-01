9921ea0e90e82905c8292442234735e1604a4c31
quaresmateo <teoian.giovane@gmail.com>
2020-08-01 12:30:54 -0300

    Verifica se produto existe

--- src/api/ranek-api/app/Controllers/Http/ImageController.js
+++ src/api/ranek-api/app/Controllers/Http/ImageController.js
@@ -23,26 +23,28 @@ class ImageController {
   }
 
   async store({ request, params }) {
-    const produto = await Produto.findBy("slug", params.id);
+    const produto = await Produto.findBy("slug", params.slug);
 
-    const images = request.file("image", {
-      types: ["image"],
-      size: "5mb"
-    });
+    if (produto) {
+      const images = request.file("image", {
+        types: ["image"],
+        size: "5mb"
+      });
 
-    await images.moveAll(Helpers.tmpPath("uploads"), file => ({
-      name: `${Date.now()}-${file.clientName}`
-    }));
+      await images.moveAll(Helpers.tmpPath("uploads"), file => ({
+        name: `${Date.now()}-${file.clientName}`
+      }));
 
-    if (!images.movedAll()) {
-      return images.errors();
+      if (!images.movedAll()) {
+        return images.errors();
+      }
+
+      await Promise.all(
+        images
+          .movedList()
+          .map(image => produto.images().create({ path: image.fileName }))
+      );
     }
-
-    await Promise.all(
-      images
-        .movedList()
-        .map(image => produto.images().create({ path: image.fileName }))
-    );
   }
 }
 
