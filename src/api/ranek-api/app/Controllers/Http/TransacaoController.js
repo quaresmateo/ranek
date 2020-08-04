2c502837949fdde8a4ac03cc35a2bad1f01de260
quaresmateo <teoian.giovane@gmail.com>
2020-08-04 15:12:30 -0300

    Implementa get e post de transações

--- src/api/ranek-api/app/Controllers/Http/TransacaoController.js
+++ src/api/ranek-api/app/Controllers/Http/TransacaoController.js
@@ -3,22 +3,36 @@
 const Database = use("Database");
 
 class TransacaoController {
-  async index({ response, request }) {
-    const type = request.all(["tipo"]);
-    let transactions = await Database.from("transacoes");
+  async index({ response, request, auth }) {
+    const { order, tipo } = request.all();
+    const user = await auth.getUser();
 
-    switch (type) {
-      case "vendedor":
-        transactions = await Database.from("transacoes").where(
-          "vendedor_id",
-          type
-        );
-        break;
-    }
+    const transactions = await Database.table("transacoes")
+      .orderBy("created_at", order ?? "asc")
+      .where(tipo, user.id);
 
-    return response.json({
-      transactions
-    });
+    const endereco = {
+      rua: user.rua,
+      cep: user.cep,
+      numero: user.numero,
+      bairro: user.bairro,
+      cidade: user.cidade,
+      estado: user.estado
+    };
+
+    transactions.endereco = { ...endereco };
+
+    return response.json({ transactions });
+  }
+
+  async store({ response, resquest }) {
+    const data = {
+      ...resquest.only(["comprador_id", "vendedor_id", "produto_id"])
+    };
+
+    const transactions = await Database.table("transacoes").insert(data);
+
+    return response.json({ transactions });
   }
 }
 
